ef82bd78e2f93776688886300549faa0166f5a74
mkrechetov <42608119+mkrechetov@users.noreply.github.com>
2021-11-14 13:03:50 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,22 +8,25 @@
 #include "graph.h"
 #include "gol.h"
 
-bool if_isomorphic_hash(Graph g, Graph h, size_t max_iter) {
+
+bool if_isomorphic_hash(Graph g, Graph h, size_t a, size_t d, size_t r, size_t max_iter) {
     if (g.adjm.size() != h.adjm.size()) {
         return false;
     }
     size_t n = g.adjm.size();
 
+    // prepare games
     std::vector<GOL> g_games, h_games;
     g_games.reserve(n);
     h_games.reserve(n);
-
     for (size_t i=0; i<n; ++i) {
         std::set<size_t> i_alive{ i };
-        g_games.emplace_back(1, 1, 1, &g, i_alive);
-        h_games.emplace_back(1, 1, 1, &h, i_alive);
+        g_games.emplace_back(a, d, r, &g, i_alive);
+        h_games.emplace_back(a, d, r, &h, i_alive);
     }
 
+    // prepare labels and hasher
+    std::hash<size_t> hasher;
     std::vector<size_t> g_labels, h_labels, g_labels_new, h_labels_new, g_labels_sorted, h_labels_sorted;
     for (size_t i=0; i<n; ++i) {
         g_labels.push_back(1);
@@ -34,14 +37,11 @@ bool if_isomorphic_hash(Graph g, Graph h, size_t max_iter) {
         h_labels_sorted.push_back(1);
     }
 
-    std::hash<size_t> hasher;
-
     for (size_t k=0; k<max_iter; ++k) {
-        for (size_t i=0; i<n; ++i) {
-            g_games[i].Evolve();
-            h_games[i].Evolve();
+        for (size_t game_index=0; game_index<n; ++game_index) {
+            g_games[game_index].Evolve();
+            h_games[game_index].Evolve();
         }
-
 
         for (size_t i=0; i<n; ++i) {
             g_labels_new[i] = g_labels[i];
@@ -66,8 +66,8 @@ bool if_isomorphic_hash(Graph g, Graph h, size_t max_iter) {
             std::sort(g_labels_sorted.begin(), g_labels_sorted.end());
             std::sort(h_labels_sorted.begin(), h_labels_sorted.end());
 
-            for (size_t jhash : g_labels_sorted) {g_labels_new[i] = hasher(g_labels_new[i] + jhash);}
-            for (size_t jhash : h_labels_sorted) {h_labels_new[i] = hasher(h_labels_new[i] + jhash);}
+            for (size_t ghash : g_labels_sorted) {g_labels_new[i] = hasher(g_labels_new[i] + ghash);}
+            for (size_t hhash : h_labels_sorted) {h_labels_new[i] = hasher(h_labels_new[i] + hhash);}
         }
 
         g_labels_sorted.clear();
@@ -94,15 +94,21 @@ bool if_isomorphic_hash(Graph g, Graph h, size_t max_iter) {
     return true;
 }
 
+
+
 void test_collection(std::string filename, size_t max_iter) {
     auto Glst = read_file(filename);
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     for (int i=0; i<Glst.size(); ++i) {
         for (int j=i+1; j<Glst.size(); ++j) {
-            if (if_isomorphic_hash(Glst[i], Glst[j], max_iter)) {
-                std::cout << "Graphs " << i << " and " << j;
-                std::cout << " coincide under GOL-test" << std::endl;
+            if (if_isomorphic_hash(Glst[i], Glst[j], 1, 1, 1, max_iter)) {
+                if (if_isomorphic_hash(Glst[i], Glst[j], 0, 1, 1, max_iter)) {
+                    if (if_isomorphic_hash(Glst[i], Glst[j], 0, 2, 1, max_iter)) {
+                        std::cout << "Graphs " << i << " and " << j;
+                        std::cout << " coincide under GOL-test" << std::endl;
+                    }
+                }
             }
         }
     }
@@ -114,6 +120,6 @@ void test_collection(std::string filename, size_t max_iter) {
 }
 
 int main() {
-    test_collection("/home/mk/CLionProjects/GameOfLifeOnGraphsCPP/collections/graph7c.g6", 5);
+    test_collection("/home/mk/CLionProjects/GameOfLifeOnGraphsCPP/collections/graph8c.g6", 5);
     return 0;
 }
